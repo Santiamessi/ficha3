e42581a77de131c4bda9deb8abab30e8dd42b0b5
Santiamessi <93229281+Santiamessi@users.noreply.github.com>
2021-10-26 17:43:56 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,31 @@
+input.onPinPressed(TouchPin.P0, function () {
+    if (true) {
+        doSomething("abc")
+        doSomething("abc")
+        Domesric_basic()
+    }
+})
+function Domesric_basic () {
+	
+}
+function Some (Yes: number) {
+	
+}
+function Lujo (Ba_k: game.LedSprite) {
+	
+}
+function doSomething (texto: string) {
+	
+}
+function Yea (Name: Image) {
+	
+}
+function Sommer () {
+    Sommer()
+}
+function Jjjib (Mate: any[]) {
+	
+}
+basic.forever(function () {
+	
+})
